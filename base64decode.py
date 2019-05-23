a19cd95a69b31961bf639889cc7fdb09a5c69c8e
winstonm <winstonmmd@gmail.com>
2019-05-23 12:39:32 -0400

    Update base64decode.py

--- base64decode.py
+++ base64decode.py
@@ -6,6 +6,6 @@ Author : Winston M
 '''
 
 import base64
-data = input("Magic starts here: ")
+data = input("Please enter the base64 encoded string here: ")
 wins = base64.b64decode(data).decode('utf-8')
 print (wins)
